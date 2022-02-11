ff446b0b5322cbe8147dd2e9811041bf620a6679
ngchinhow <ngchinhow@gmail.com>
2022-02-11 12:50:44 +0800

    Initial deploy

--- server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipeRestController.java
+++ server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipeRestController.java
@@ -1,3 +1,62 @@
 package ibf2021.assessment.csf.server.controllers;
 
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ibf2021.assessment.csf.server.models.Recipe;
+import ibf2021.assessment.csf.server.services.RecipeService;
+
 /* Write your request hander in this file */
+@RestController
+@RequestMapping(path = "/api")
+public class RecipeRestController {
+    @Autowired
+    private RecipeService rSvc;
+	private final Logger logger = Logger.getLogger(RecipeRestController.class.getName());
+
+
+    @GetMapping(
+        path = "/recipe/{recipeId}",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> getRecipeById(
+        @PathVariable(name = "recipeId", required = true) String id
+    ) {
+        Recipe recipe = rSvc.getRecipeById(id).orElse(null);
+        if (recipe == null) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Could not find recipe with id " + id
+            );
+        }
+        return new ResponseEntity<String>(
+            recipe.toString(),
+            HttpStatus.OK
+        );
+    }
+
+    @PostMapping(
+        path = "/recipe",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> addRecipe(@RequestBody Recipe recipe) {
+        logger.info(recipe.toString());
+        rSvc.addRecipe(recipe);
+        return new ResponseEntity<String>(
+            recipe.toString(),
+            HttpStatus.CREATED
+        );
+    }
+}
--- server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipesRestController.java
+++ server/src/main/java/ibf2021/assessment/csf/server/controllers/RecipesRestController.java
@@ -1,4 +1,42 @@
 package ibf2021.assessment.csf.server.controllers;
 
-/* Write your request hander in this file */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import ibf2021.assessment.csf.server.services.RecipeService;
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObjectBuilder;
+
+/* Write your request hander in this file */
+@RestController
+@RequestMapping(path = "/api")
+public class RecipesRestController {
+
+    @Autowired
+    private RecipeService rSvc;
+
+    @GetMapping(
+        path = "/recipes",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> getAllRecipes() {
+        JsonArrayBuilder responseBuilder = Json.createArrayBuilder();
+        rSvc.getAllRecipes()
+            .forEach(r -> {
+                JsonObjectBuilder partialRecipeBuilder = Json.createObjectBuilder();
+                partialRecipeBuilder.add("id", r.getId());
+                partialRecipeBuilder.add("title", r.getTitle());
+                responseBuilder.add(partialRecipeBuilder);
+            });
+        return new ResponseEntity<String> (
+            responseBuilder.build().toString(),
+            HttpStatus.OK
+        );
+    }
+}
